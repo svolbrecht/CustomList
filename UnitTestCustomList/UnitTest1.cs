41554480da4e7907b7401deb47aca193566f3833
StevenRandallVolbrecht <svolbrecht@yahoo.com>
2018-09-09 16:34:56 -0500

    unit tests for zip method added

--- UnitTestCustomList/UnitTest1.cs
+++ UnitTestCustomList/UnitTest1.cs
@@ -270,16 +270,66 @@ namespace UnitTestCustomList
         }
 
         [TestMethod]
-        public void ConvertListToString()
+        public void CheckConvertListToString()
         {
-            CustomList<int> test1 = new CustomList<int>();
             //Arrange
+            CustomList<int> test1 = new CustomList<int>();
             test1.Add(1);
             test1.Add(2);
             test1.Add(3);
             string expectedValue = "1,2,3";
+
             //Act
             string actualValue = test1.ToString();
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void CheckZipTwoLists()
+        {
+            //Arrange
+            CustomList<int> test1 = new CustomList<int>();
+            CustomList<int> test2 = new CustomList<int>();
+            CustomList<int> test3 = new CustomList<int>();
+            test1.Add(1);
+            test1.Add(3);
+            test1.Add(5);
+            test2.Add(2);
+            test2.Add(4);
+            test2.Add(6);
+            int expectedValue = 5;
+
+            //Act
+            test3 = test1.Zip(test2);
+            int actualValue = test3[4];
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void CheckZipListOfDifferentSizes()
+        {
+            //Arrange
+            CustomList<int> test1 = new CustomList<int>();
+            CustomList<int> test2 = new CustomList<int>();
+            CustomList<int> test3 = new CustomList<int>();
+            test1.Add(1);
+            test1.Add(3);
+            test1.Add(5);
+            test2.Add(2);
+            test2.Add(4);
+            test2.Add(6);
+            test2.Add(7);
+            test2.Add(8);
+            int expectedValue = 8;
+
+            //Act
+            test3 = test1.Zip(test2);
+            int actualValue = test3[7];
+
             //Assert
             Assert.AreEqual(expectedValue, actualValue);
         }
